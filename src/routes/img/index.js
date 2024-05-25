38b57b35164f10cf206788139bc9a4be114d978b
Pedro Pablo <pmontesarroyave@gmail.com>
2024-05-24 23:20:45 -0500

    mostramos la ultima foto

--- src/routes/img/index.js
+++ src/routes/img/index.js
@@ -17,11 +17,8 @@ router.get('/', upload.single('photo'), uploadPhoto);
 
 //visualizar la imagen
 router.get('/id/:id', (req, res) => {
-  1
   try {
     const filePath = path.resolve(__dirname, `../../../public/img/${req.params.id}`);
-
-    // res.sendFile(`public/img/${req.params.id}`, { root: '.' });
     res.sendFile(filePath);
 
   } catch (error) {
@@ -34,7 +31,6 @@ router.get('/id/:id', (req, res) => {
 router.get('/list', (req, res) => {
   try {
     const path = './public/img';
-
     //leemos los nombre de todos archivos en la carpeta img
     fs.readdir(path, (err, files) => {
       if (err) {
@@ -44,7 +40,26 @@ router.get('/list', (req, res) => {
         res.status(200).send(files);
       }
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: 'Error al listar las imágenes' });
+  }
+})
 
+//ver el ulimo nombre de la imagen
+router.get('/last', (req, res) => {
+  try {
+    const path = './public/img';
+    //leemos los nombre de todos archivos en la carpeta img
+    fs.readdir(path, (err, files) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send({ message: 'Error al listar las imágenes' });
+      } else {
+        const file = files[files.length - 1];
+        res.sendFile(file, { root: 'public/img' });
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: 'Error al listar las imágenes' });
