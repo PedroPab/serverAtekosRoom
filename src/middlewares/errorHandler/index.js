10d3378ea7137f0f525235ed2bf2e2a8cbd021ec
Pedro Pablo <pmontesarroyave@gmail.com>
2024-06-10 22:49:14 -0500

    feat: Remove commented code and console.error statements in errorHandler middleware

--- src/middlewares/errorHandler/index.js
+++ src/middlewares/errorHandler/index.js
@@ -15,7 +15,7 @@ export const errorHandler = (err, req, res, next) => {
   Logs.logInfo(`[errorHandler]`)
 
   const statusCode = err.statusCode || 500;
-  console.error(err.message, err.stack);
+  // console.error(err.message, err.stack);
   res.status(statusCode).json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
