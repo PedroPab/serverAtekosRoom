8ea8e0ae21fd15f6863d79c1e25ec5496b41dc98
Pedro Pablo <pmontesarroyave@gmail.com>
2024-07-19 10:14:04 -0500

    Revert "lsugo lo borro"
    This reverts commit 7a238da10044559c8ac9cdb47516ebbcb95851db.

--- src/config/index.js
+++ src/config/index.js
@@ -15,7 +15,6 @@ console.log(`corriendo en ${environment}`);
 const serviceAccountJson = process.env.SERVICES_KEY_BUCKET || '{}';
 const serviceAccount = JSON.parse(serviceAccountJson);
 
-
 export default {
   NODE_ENV: process.env.NODE_ENV || 'development',
   PORT: process.env.PORT || 3000,
@@ -26,14 +25,3 @@ export default {
   BUCKET_NAME: process.env.BUCKET_NAME || 'server-bucket',
   SERVICES_KEY_BUCKET: serviceAccount
 };
-
-console.log({
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000,
-  HOST: process.env.HOST || 'localhost',
-  //Config the bucket google cloud storage
-  PROJECT_ID: process.env.PROJECT_ID || 'atekos',
-  // KEY_FILE_NAME: process.env.KEY_FILE_NAME || '.servicesKeyBucket.json',
-  BUCKET_NAME: process.env.BUCKET_NAME || 'server-bucket',
-  SERVICES_KEY_BUCKET: serviceAccount
-})
