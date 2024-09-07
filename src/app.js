3aac61b3a86649e46678250f957f83f93bfd2c90
Pedro Pablo <pmontesarroyave@gmail.com>
2024-09-07 00:04:04 -0500

    feat: Agregar manejo de error 404 para rutas no encontradas

--- src/app.js
+++ src/app.js
@@ -12,6 +12,12 @@ middlewares(app);
 //aplicar las rutas
 router(app);
 
+//404 not found
+app.use((req, res) => {
+  //mandar la pagina de public/html/404.html
+  res.status(404).sendFile('404.html', { root: 'public/html' });
+});
+
 //middlewares para errores
 middlewaresErrors(app);
 
