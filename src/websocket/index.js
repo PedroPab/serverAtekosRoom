c413bb9470119c905e97e6ac2144d08ac8f2594f
Pedro Pablo <pmontesarroyave@gmail.com>
2024-11-13 22:40:02 -0500

    feat: mejorar la gestión de suscripciones y eventos en WebSocket, incluyendo actualizaciones y creación de salas

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -1,7 +1,9 @@
+// setupWebSocket.js
 import { WebSocketServer } from 'ws'
 import Logs from '../utils/logColor/index.js'
 import clients from './clients/index.js'
 import subscriptions from './subscriptions/index.js'
+import { eventRoom } from '../rooms/infrastructure/dependencies/eventRoom.js' // Importamos eventRoom
 
 export default function setupWebSocket(server) {
 
@@ -9,32 +11,37 @@ export default function setupWebSocket(server) {
 
 	wss.on('connection', (ws, req) => {
 		const remoteAddress = req.socket.remoteAddress
-		Logs.logInfo(`se conecto un nuevo websocket ${remoteAddress}`)
+		Logs.logInfo(`Se conectó un nuevo WebSocket ${remoteAddress}`)
 
 		let userId // Identificador único del cliente
+		let event // Evento al que se suscribe el cliente
+		let roomId // Room al que se suscribe el cliente
+		// let message // Mensaje recibido del cliente
 
 		ws.on('message', (message) => {
-			//recibit buffer y parsear a json
-			const msg = JSON.parse(message)
+			const msg = JSON.parse(message) // Parsear el buffer a JSON
+			//id del cliente socket
+			userId = ws._socket._handle.fd
 
 			switch (msg.type) {
 				case 'identify':
-					userId = msg.userId
 					clients[userId] = ws
-					Logs.logSuccess(`el cliente se identifica como ${userId}`)
-					ws.send(`Te identificaste como ${userId}, suscribete a un evento o espera a que te volvamos a mandar un mensaje xd`)
+					Logs.logSuccess(`El cliente se identifica como ${userId}`)
+					ws.send(`Te identificaste como ${userId}, suscríbete a un evento o espera actualizaciones de Room.`)
 					break
 				case 'subscribe':
-					// Añadir este cliente a la lista de suscripciones para el evento especificado
-					Logs.logSuccess(`el cliente ${userId} se suscribió al evento ${msg.event}`)
-					if (subscriptions[msg.event]) {
-						subscriptions[msg.event].push(userId)
-					} else {
-						subscriptions[msg.event] = [userId]
-					}
+					event = msg.event
+					roomId = msg.room
+
+					Logs.logSuccess(`El cliente ${userId} se suscribió al evento ${event} para el room ${roomId}`)
+
+					// Verificar si existe el evento y el room, y añadir el usuario de forma más concisa
+					subscriptions[msg.event] = subscriptions[msg.event] || {}
+					subscriptions[msg.event][roomId] = subscriptions[msg.event][roomId] || []
+					subscriptions[msg.event][roomId].push(userId)
+
 					break
 				case 'message':
-					// Enviar el mensaje a todos los clientes suscritos al evento especificado
 					if (subscriptions[msg.event]) {
 						subscriptions[msg.event].forEach(subscriber => {
 							clients[subscriber].send(JSON.stringify(msg))
@@ -48,8 +55,8 @@ export default function setupWebSocket(server) {
 		})
 
 		ws.on('close', () => {
-			Logs.logError(`se desconecto el websocket ${remoteAddress}, con el id ${userId}`)
-			// Limpiar al cliente de todas las suscripciones y la lista de clientes
+			Logs.logError(`Se desconectó el WebSocket ${remoteAddress}, con el id ${userId}`)
+			// Limpiar al cliente de todas las suscripciones y de la lista de clientes
 			Object.keys(subscriptions).forEach(event => {
 				subscriptions[event] = subscriptions[event].filter(subscriber => subscriber !== userId)
 			})
@@ -57,6 +64,34 @@ export default function setupWebSocket(server) {
 		})
 	})
 
-	return wss
+	// Escuchar eventos de Room y enviarlos a clientes suscritos
+	eventRoom.on('room.update', (data) => {
+		Logs.logInfo(`Evento de actualización de Room recibido para ${data.id}`)
+		const roomId = data.id
+		//miramos los que están suscritos a room.update del room que se actualizo
 
+		const event = 'room.update'
+		if (subscriptions[event]) {
+
+			subscriptions[event][roomId].forEach(subscriber => {
+				if (clients[subscriber]) {
+					clients[subscriber].send(JSON.stringify({ event, data }))
+				}
+			})
+		}
+	})
+
+	eventRoom.on('room.create', (data) => {
+		Logs.logInfo(`Evento de creación de Room recibido para ${data.id}`)
+		const event = 'room.create'
+		if (subscriptions[event]) {
+			subscriptions[event].forEach(subscriber => {
+				if (clients[subscriber]) {
+					clients[subscriber].send(JSON.stringify({ event, data }))
+				}
+			})
+		}
+	})
+
+	return wss
 }
